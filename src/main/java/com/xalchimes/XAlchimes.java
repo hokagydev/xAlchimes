85ae3fbd4acf172f3d84f1a8d5af3d539dfc616c
HokagyDev <l79142829@gmail.com>
2026-08-07 15:00:45 +0300

    Update XAlchimes.java

--- src/main/java/com/xalchimes/XAlchimes.java
+++ src/main/java/com/xalchimes/XAlchimes.java
@@ -3,10 +3,13 @@ package com.xalchimes;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class XAlchimes extends JavaPlugin {
+    private AlchemyGUI guiHandler;
+    
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new AlchemyListener(), this);
-        getCommand("alchemy").setExecutor(new AlchemyCommand());
+        this.guiHandler = new AlchemyGUI();
+        getServer().getPluginManager().registerEvents(guiHandler, this);
+        getCommand("alchemy").setExecutor(new AlchemyCommand(guiHandler));
         getServer().getConsoleSender().sendMessage("§aXAlchimes enabled!");
     }
 
