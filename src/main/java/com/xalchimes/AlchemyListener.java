970caeff0d83471b6ba2ec69196a673f6ef3dfb5
HokagyDev <l79142829@gmail.com>
2026-08-07 15:01:45 +0300

    Refactor AlchemyListener by removing unused constructor
    Removed the constructor and registration of guiHandler in AlchemyListener.

--- src/main/java/com/xalchimes/AlchemyListener.java
+++ src/main/java/com/xalchimes/AlchemyListener.java
@@ -1,13 +1,6 @@
 package com.xalchimes;
 
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class AlchemyListener implements Listener {
-    private final AlchemyGUI guiHandler;
-    
-    public AlchemyListener() {
-        this.guiHandler = new AlchemyGUI(null);
-        JavaPlugin.getPlugin(XAlchimes.class).getServer().getPluginManager().registerEvents(guiHandler, JavaPlugin.getPlugin(XAlchimes.class));
-    }
 }
