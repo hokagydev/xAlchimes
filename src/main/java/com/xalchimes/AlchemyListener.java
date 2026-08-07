cf916e7ca1ece5070960ec2b5a931ce6c7335af7
HokagyDev <l79142829@gmail.com>
2026-08-07 14:55:56 +0300

    Refactor AlchemyListener class implementation
    Refactor AlchemyListener to use Listener interface and initialize guiHandler properly.

--- src/main/java/com/xalchimes/AlchemyListener.java
+++ src/main/java/com/xalchimes/AlchemyListener.java
@@ -1,11 +1,13 @@
 package com.xalchimes;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class AlchemyListener implements org.bukkit.event.Listener {
-    private final AlchemyGUI guiHandler = new AlchemyGUI(null);
+public class AlchemyListener implements Listener {
+    private final AlchemyGUI guiHandler;
     
     public AlchemyListener() {
+        this.guiHandler = new AlchemyGUI(null);
         JavaPlugin.getPlugin(XAlchimes.class).getServer().getPluginManager().registerEvents(guiHandler, JavaPlugin.getPlugin(XAlchimes.class));
     }
 }
