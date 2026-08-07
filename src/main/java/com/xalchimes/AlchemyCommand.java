9b8cb3022d3bafcc62143714d14aad8a39a90254
HokagyDev <l79142829@gmail.com>
2026-08-07 15:00:59 +0300

    Refactor AlchemyCommand to use guiHandler

--- src/main/java/com/xalchimes/AlchemyCommand.java
+++ src/main/java/com/xalchimes/AlchemyCommand.java
@@ -6,13 +6,19 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class AlchemyCommand implements CommandExecutor {
+    private final AlchemyGUI guiHandler;
+    
+    public AlchemyCommand(AlchemyGUI guiHandler) {
+        this.guiHandler = guiHandler;
+    }
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player player)) {
             sender.sendMessage("§cOnly players can use this command!");
             return true;
         }
-        new AlchemyGUI(player).open();
+        guiHandler.open(player);
         return true;
     }
 }
